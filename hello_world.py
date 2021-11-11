814a190b75de1ae94b590c739b309bcee938723b
mlestay <80291528+mlestay@users.noreply.github.com>
2021-11-11 18:42:19 -0300

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+print("hola Mundo")
+name = "Marlene"
+print( "hola", name) 
+print ( "hola" + " " + name)
+fav_num = int(7)
+print ("hola", fav_num)
+print ("hola" + str(fav_num))
+fav_food1 = "puré"
+fav_food2 = "bistec"
+print( f"Me encanta comer {fav_food1} con {fav_food2}")
+print ("Me encanta comer {} con {}".format(fav_food1,fav_food2))
